cc012fbfcb1994288ab899c954332262d02be29b
Antonio Almudévar <91321976+antonioalmudevar@users.noreply.github.com>
2022-03-15 13:51:28 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(  name='vibration',
+        version='1.0',
+        description='Anomalous vibration detection',
+        author='Antonio Almudevar',
+        author_email='almudevar@unizar.es',
+        packages=['vibration'],
+    )
